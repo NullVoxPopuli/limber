2ab15ecbf192ede2305405db1076d7ca43d9c5e8
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-20 19:05:59 -0400

    Change Header

--- config/icons.js
+++ config/icons.js
@@ -1,12 +1,13 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function () {
   return {
     'free-solid-svg-icons': [
       'external-link-alt'
     ],
     'free-brands-svg-icons': [
-      'ember'
+      'ember',
+      'markdown'
     ]
   }
 }
