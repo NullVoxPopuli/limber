f9b47d7025b508d97a1778d5900d0b9d09515fef
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-26 21:46:00 -0400

    Update the snippet's todos and add a gjs example that still needs coloring

--- app/services/starting-snippet.js
+++ app/services/starting-snippet.js
@@ -1,14 +1,19 @@
 export const DEFAULT_SNIPPET = `
 # Limber Editor
 
-_Ember rendered with markdown_
+**glimdown** //  _Ember or Glimmer  rendered with markdown_
 
 Similar to MDX!
+
+## TODOs
+
+- bundle [babel][1]
+- bundle [ember-template-imports][2]
+- add new [glimdown syntax][3] to various editors
 
 ## Template only content
 
 List of links:
-
 <ul>
   {{#each (array
     (hash href='https://emberjs.com' text='Ember home page')
@@ -23,10 +28,26 @@ List of links:
 
 ## With JavaScript
 
-TBD (need to add babel to the browser bundle)
+\`\`\`gjs live
+import Component, { tracked } from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { on } from '@ember/modifier';
 
-## TODOs
+export default class HelloWorld extends Component {
+  @tracked count = 1;
 
-- dynamically load template, babel, and markdown compilers
-- dynamically load code editor
+  increment = () => this.count++;
+
+  <template>
+    <p>You have clicked the button {{this.count}} times.</p>
+
+    <button {{on "click" this.increment}}>Click</button>
+  </template>
+}
+\`\`\`
+
+[1]: https://github.com/glimmerjs/glimmer-experimental/tree/master/packages/examples/playground
+[2]: https://github.com/ember-template-imports/ember-template-imports
+[3]: https://github.com/NullVoxPopuli/limber/issues/14
 `.trim();
