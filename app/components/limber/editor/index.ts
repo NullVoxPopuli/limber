b0e7f4bd5022896b31cb3595840846c7a8c3255d
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-03-27 22:20:57 -0400

    Dynamically load monaco-editor's base js to make initial load faster

--- app/components/limber/editor/index.ts
+++ app/components/limber/editor/index.ts
@@ -3,11 +3,23 @@ import { tracked } from '@glimmer/tracking';
 
 import { modifier } from 'ember-could-get-used-to-this';
 
+import monaco, { setupMonaco } from './monaco';
+
 export default class Editor extends Component {
   @tracked isEditing = false;
+  @tracked isLoading = false;
+
+  monaco = monaco;
 
   waitForUser = modifier(() => {
-    const activate = () => (this.isEditing = true);
+    const activate = async () => {
+      this.isLoading = true;
+
+      await setupMonaco();
+
+      this.isEditing = true;
+      this.isLoading = false;
+    };
 
     window.addEventListener('mousemove', activate);
     window.addEventListener('keydown', activate);
