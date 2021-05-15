cdc07db53e2edfc16bc3b2cf74af131bdaab3739
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-05-14 20:45:44 -0400

    Aggressively catch errors

--- tests/cypress/integration/editor-loading.spec.js
+++ tests/cypress/integration/editor-loading.spec.js
@@ -7,38 +7,44 @@ const PROD = 'https://limber.glimdown.com';
 
 const ORIGIN = Cypress.env('DEPLOY_URL') || PROD;
 
-describe('Loads Monaco', () => {
+describe('Editor Loading', () => {
   beforeEach(() => {
+    /**
+     * For every test, we need to ignore exceptions, because
+     * during subsequent tests, browserstack loads the previous page...
+     */
     cy.on('uncaught:exception', (err, runnable) => {
       // Monaco is throwing an error about top level export
       // for some reason
-      // if (err.message.includes('export')) {
-      //   // returning false here prevents Cypress from
-      //   // failing the test
-      //   return false;
-      // }
+      if (err.message.includes('export')) {
+        // returning false here prevents Cypress from
+        // failing the test
+        return false;
+      }
 
       return false;
     });
   });
 
-  it('loads with no errors', () => {
-    cy.visit(`${ORIGIN}${MONACO_PATH}`);
+  describe('Loads Monaco', () => {
+    it('loads with no errors', () => {
+      cy.visit(`${ORIGIN}${MONACO_PATH}`);
 
-    cy.get('body').trigger('mousemove');
-    cy.get('.monaco-editor', { timeout: 20000 }).should('be.visible');
-    cy.get('.cm-editor').should('not.exist');
-    cy.get('[data-test-loading-error]').should('not.exist');
+      cy.get('body').trigger('mousemove');
+      cy.get('.monaco-editor', { timeout: 20000 }).should('be.visible');
+      cy.get('.cm-editor').should('not.exist');
+      cy.get('[data-test-loading-error]').should('not.exist');
+    });
   });
-});
 
-describe('Loads CodeMirror', () => {
-  it('loads with no errors', () => {
-    cy.visit(`${ORIGIN}${CODEMIRROR_PATH}`);
+  describe('Loads CodeMirror', () => {
+    it('loads with no errors', () => {
+      cy.visit(`${ORIGIN}${CODEMIRROR_PATH}`);
 
-    cy.get('body').trigger('mousemove');
-    cy.get('.cm-editor', { timeout: 20000 }).should('be.visible');
-    cy.get('.monaco-editor').should('not.exist');
-    cy.get('[data-test-loading-error]').should('not.exist');
+      cy.get('body').trigger('mousemove');
+      cy.get('.cm-editor', { timeout: 20000 }).should('be.visible');
+      cy.get('.monaco-editor').should('not.exist');
+      cy.get('[data-test-loading-error]').should('not.exist');
+    });
   });
 });
