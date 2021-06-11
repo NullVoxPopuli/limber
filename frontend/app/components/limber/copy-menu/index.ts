92c92220e8764563e6f94f7b8ce2492eb4989bfc
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-06-11 11:04:30 -0400

    Just kidding, need popper for positioning, because headless tailwind ui, isn't concerned with positioning

--- frontend/app/components/limber/copy-menu/index.ts
+++ frontend/app/components/limber/copy-menu/index.ts
@@ -32,46 +32,53 @@ class CopyMenu extends Component {
 export default setComponentTemplate(
   hbs`
     <Menu class="absolute top-3 right-4 z-10" data-test-copy-menu ...attributes as |menu|>
-      <menu.Button
-        class="
-          text-black
-          relative rounded-sm border border-gray-900 bg-white px-2 py-1 -my-1 text-left
-          transition ease-in-out duration-150 sm:text-sm
-          focus:ring-4 focus-visible:outline-none ring-ember-brand focus:outline-none
-        "
-    >
-      📋
-    </menu.Button>
-    <menu.Items class="absolute z-10 grid mt-1 rounded-sm bg-white shadow-lg" as |items|>
-      <items.Item as |item|>
-        <item.Element
-          {{on 'click' this.copyAsText}}
-          @tagName="button"
+      <PopperJS as |trigger popover|>
+        <menu.Button
+          {{trigger}}
           class="
-            bg-transparent
-            block w-full select-none relative py-2 px-4 text-left
-            text-black hover:bg-gray-100 focus:ring-4 ring-inset
+            text-black
+            relative rounded-sm border border-gray-900 bg-white px-2 py-1 -my-1 text-left
+            transition ease-in-out duration-150 sm:text-sm
+            focus:ring-4 focus-visible:outline-none ring-ember-brand focus:outline-none
           "
-          tabindex="0"
-        >
-          Copy as text
-        </item.Element>
-      </items.Item>
-      <items.Item as |item|>
-        <item.Element
-          {{on 'click' this.copyAsText}}
-          @tagName="button"
-          class="
-            bg-transparent
-            block w-full select-none relative py-2 px-4 text-left
-            text-black hover:bg-gray-100 focus:ring-4 ring-inset
-          "
-          tabindex="0"
-        >
-          Copy as image
-        </item.Element>
-      </items.Item>
-    </menu.Items>
+      >
+        📋
+      </menu.Button>
+      <menu.Items
+        {{popover}}
+        class="absolute top-2 z-10 grid mt-1 rounded-sm bg-white shadow-lg min-w-max"
+        as |items|
+      >
+        <items.Item as |item|>
+          <item.Element
+            {{on 'click' this.copyAsText}}
+            @tagName="button"
+            class="
+              bg-transparent
+              block w-full select-none relative py-2 px-4 text-left
+              text-black hover:bg-gray-100 focus:ring-4 ring-inset
+            "
+            tabindex="0"
+          >
+            Copy as text
+          </item.Element>
+        </items.Item>
+        <items.Item as |item|>
+          <item.Element
+            {{on 'click' this.copyAsText}}
+            @tagName="button"
+            class="
+              bg-transparent
+              block w-full select-none relative py-2 px-4 text-left
+              text-black hover:bg-gray-100 focus:ring-4 ring-inset
+            "
+            tabindex="0"
+          >
+            Copy as image
+          </item.Element>
+        </items.Item>
+      </menu.Items>
+    </PopperJS>
   </Menu>
   `,
   CopyMenu
---
+++ frontend/app/components/popper-j-s.ts
@@ -0,0 +1,84 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { setComponentTemplate } from '@ember/component';
+import { registerDestructor } from '@ember/destroyable';
+import { action } from '@ember/object';
+import { hbs } from 'ember-cli-htmlbars';
+
+import { createPopper } from '@popperjs/core';
+// could use ember-modifier here as well
+import { modifier } from 'ember-could-get-used-to-this';
+
+import type Popper from '@popperjs/core';
+
+interface Args {
+  placement?: Popper.Placement;
+}
+
+// const showEvents = ['mouseenter', 'focus', 'click'];
+// const hideEvents = ['mouseleave', 'blur'];
+
+class PopperJS extends Component<Args> {
+  declare _triggerElement?: HTMLElement;
+  declare _popoverElement?: HTMLElement;
+
+  @tracked isShown = false;
+
+  trigger = modifier((element: HTMLElement) => {
+    this._triggerElement = element;
+
+    if (this._popoverElement) this.setupPopper();
+  });
+
+  popover = modifier((element: HTMLElement) => {
+    this._popoverElement = element;
+
+    if (this._triggerElement) this.setupPopper();
+  });
+
+  @action
+  setupPopper() {
+    const { _popoverElement: popover, _triggerElement: trigger } = this;
+    const { placement } = this.args;
+
+    if (!popover) return;
+    if (!trigger) return;
+
+    let popper = createPopper(trigger, popover, {
+      placement: placement || 'bottom-end',
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
+        },
+      ],
+    });
+
+    // let hide = () => (this.isShown = false);
+    // let show = () => {
+    //   this.isShown = true;
+    //   popper.update();
+    // };
+
+    // popper.update();
+
+    // showEvents.forEach((event) => {
+    //   trigger.addEventListener(event, show);
+    // });
+
+    // hideEvents.forEach((event) => {
+    //   trigger.addEventListener(event, hide);
+    // });
+
+    registerDestructor(this, () => popper.destroy());
+  }
+}
+
+export default setComponentTemplate(
+  hbs`
+    {{yield this.trigger this.popover this.isShown}}
+  `,
+  PopperJS
+);
