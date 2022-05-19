b13bb934736dd749eb513a0f1f54756476737032
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-05-19 09:40:20 -0400

    Use released package of turborepo-remote-cache

--- launch-daemon.sh
+++ launch-daemon.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 
-# can also be ran without docker
-# as the original project is written
-# in typescript
-# - node
-# - Fastify: https://www.fastify.io/
-
 # On my test system, I didn't feel like setting up docker
 # docker run \
 #   --env-file=.env.turbo \
 #   -p 3000:13000 \
 #   fox1t/turborepo-remote-cache
+#
+# We must provide a shell script to pm2,
+# because pm2 doesn't fallback to trying shell eecution
+# when the script doesn't happen to be js.
+#
+# maybe there are better daemon tools than pm2 out there.
 
-# This runs directly from a git clone (node)
-# Before running this,
-#  - clone the repo
-#  - npm install
-#  - npm run prebuild
-#  - npm run build
 (\
   set -a \
   && . ./.env.turbo \
-  && ( cd ../turborepo-remote-cache && npm run start ) \
+  && ./node_modules/.bin/turborepo-remote-cache \
 )
