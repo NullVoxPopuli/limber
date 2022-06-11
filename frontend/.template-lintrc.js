0a1f143ae32a2d3f87f6b6ed4be64c1c5df8e411
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-06-10 23:12:49 -0400

    It works

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   plugins: ['ember-template-lint-plugin-prettier'],
-  extends: ['recommended', 'ember-template-lint-plugin-prettier'],
+  extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
 };
