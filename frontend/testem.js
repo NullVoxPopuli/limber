62c2e019917093d01d40eae4b92e6f7b20eb7182
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-25 20:00:21 -0400

    lint:fix

--- frontend/testem.js
+++ frontend/testem.js
@@ -6,10 +6,10 @@ const CI_BROWSER = process.env.CI_BROWSER || DEFAULT_BROWSER;
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  "proxies": {
-    "/output": {
-      "target": "http://localhost:4200"
-    }
+  proxies: {
+    '/output': {
+      target: 'http://localhost:4200',
+    },
   },
   launch_in_ci: [CI_BROWSER],
   launch_in_dev: [DEFAULT_BROWSER],
