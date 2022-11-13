fe81fc79130065ca28dd105b48107d2bd4675d84
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-11-13 12:02:29 -0500

    Use turbo for everything, use Changeset for releasing the new addon

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,17 @@
+'use strict';
+
+const { configs } = require('@nullvoxpopuli/eslint-configs');
+const { baseConfig: baseNode } = require('@nullvoxpopuli/eslint-configs/configs/node');
+
+const config = configs.ember();
+
+module.exports = {
+  ...config,
+  overrides: [
+    ...config.overrides,
+    {
+      ...baseNode,
+      files: ['browserstack.testem.js'],
+    },
+  ],
+};
