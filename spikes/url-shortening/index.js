79186b0eb584a7324ef7004543018c0e0c58bfe0
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-01-06 14:59:37 -0500

    Add benchmarking

--- spikes/url-shortening/index.js
+++ spikes/url-shortening/index.js
@@ -1,10 +1,10 @@
-import JSONCrush from 'jsoncrush'
-import * as huffman from '@algorithm.ts/huffman'
-import base64 from '@algorithm.ts/base64'
-import {filesize} from "filesize";
-import LZString from 'lz-string';
+import { filesize } from "filesize";
+import { Bench } from 'tinybench';
 
 import { originalText } from './data.js'
+import * as huffman from './techniques/huffman.js';
+import * as JSONCrush from './techniques/json-crush.js';
+import * as LZString from './techniques/lz-string.js';
 
 const size = (str) => filesize(str.length, { base: 2, standard: "jedec"})
 
@@ -20,49 +20,51 @@ const bundleImpact = {
   [names.lzString]: '5.17KB (1.61KB gzip)'
 }
 
-
 console.log(
   [
     ['original', originalText],
-    ['JSONCrush', JSONCrush.crush(JSON.stringify({ i: originalText }))],
-    ['Huffman', compress(originalText)],
-    ['lz-string', LZString.compressToBase64(originalText),
-      LZString.compressToEncodedURIComponent(originalText)]
+    ['JSONCrush', JSONCrush.encode(originalText)],
+    ['Huffman', huffman.encode(originalText)],
+    ['lz-string', LZString.encode(originalText)]
 
-  ].map(([label, text, uriText]) => {
-    return `
+  ].map(([label, text]) => {
+    return `\n
       ${label}: ${size(text)}
-      ${label} as URI: ${size(uriText ?? encodeURIComponent(text))}
-    `
+      ${label} as URI: ${size(encodeURIComponent(text))}`
   }).join('')
-)
+);
 
+const encodeBench = new Bench({ time: 100 });
+const decodeBench = new Bench({ time: 100 });
 
-/**
-  * The Huffman library requires a lot of boilerplate.
-  */
-function compress(text) {
-  const { encodedData, encodingTable } = huffman.encode(text)
-  const cipherBuffer = huffman.compress(encodedData)
-  const cipherText = base64.encode(cipherBuffer)
+encodeBench
+  .add('JSONCrush', () => JSONCrush.encode(originalText))
+  .add('Huffman', () => huffman.encode(originalText))
+  .add('lz-string', () => LZString.encode(originalText));
 
-  const textEncoder = new TextEncoder('utf-8')
-  const encodingTableText = base64.encode(
-    textEncoder.encode(JSON.stringify(compressEncodingTable(encodingTable))),
-  )
-  const result = (encodingTableText + '-' + cipherText)
-    .replace(/\//g, '(')
-    .replace(/\+/g, ')')
-    .replace(/=/g, '_')
-  return result
+const encodedText = {
+  JSONCrush: JSONCrush.encode(originalText),
+  huffman: huffman.encode(originalText),
+  LZString: LZString.encode(originalText),
 }
-function compressEncodingTable(table){
-  const entries = Object.entries(table)
-    .map(([value, path]) => {
-      let p = 1
-      for (const x of path) p = (p << 1) | x
-      return [value, p]
-    })
-    .flat()
-  return entries
-}
+
+decodeBench
+  .add('JSONCrush', () => JSONCrush.decode(encodedText.JSONCrush))
+  .add('Huffman', () => huffman.decode(encodedText.huffman))
+  .add('lz-string', () => LZString.decode(encodedText.LZString));
+
+const printBench = (bench) => console.table(
+  bench.tasks.map(({ name, result }) => ({
+    "Task Name": name,
+    "Average Time (ps)": result?.mean * 1000,
+    "Variance (ps)": result?.variance * 1000
+  }))
+);
+
+await encodeBench.run();
+await decodeBench.run();
+
+console.log('Encode benchmark:')
+printBench(encodeBench);
+console.log('Decode benchmark:')
+printBench(decodeBench);
