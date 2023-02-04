3886711049782c7031984bdcbdb085a4f621b472
NullVoxPopuli <LPSego3+dev@gmail.com>
2023-02-04 11:32:15 -0500

    More dev commands to help sync the workspaces

--- dev/index.js
+++ dev/index.js
@@ -2,7 +2,9 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+
 import { symlinkEverywhere } from './commands/symlink-everywhere.js';
+import { syncDeps } from './commands/sync-deps.js';
 
 
 let yarg = yargs(hideBin(process.argv));
@@ -21,5 +23,31 @@ yarg
     },
     symlinkEverywhere
   )
+.command(
+  'sync-deps <depName>',
+  'updates a depenedncy to the latest version, if it exists.', 
+  (yargs) => {
+    yargs.positional('depName', {
+      description: 'The dep name to update',
+      required: true,
+    });
+    yargs.option('force', {
+      default: false,
+      type: 'boolean',
+      description: `add the dependency even if an older version isn't in the package.json`
+    });
+    yargs.option('dev', {
+      default: true,
+      type: 'boolean',
+      description: 'add the dep as a devDependency',
+    });
+    yargs.option('range', {
+      default: '^',
+      type: 'string',
+      description: 'the range to use for the version. Defaults to ^. Can be ~, ^, or "pin"'
+    })
+  },
+  syncDeps,
+)
   .demandCommand()
   .help().argv;
