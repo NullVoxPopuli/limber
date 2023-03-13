ed8d20c82fed40cdeb34748de889992f35457082
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-03-12 20:58:02 -0400

    Update bot-push.sh

--- dev/bot-push.sh
+++ dev/bot-push.sh
@@ -6,6 +6,6 @@ git checkout -b $branch
 git config user.name "github-actions[bot]"
 git config user.email "github-actions-bot@users.noreply.github.com"
 git add .
-git commit -m "Automated updated"
+git commit -m "Automated update"
 git push origin $branch --force 
 
