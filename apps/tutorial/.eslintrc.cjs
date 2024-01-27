cffd9790eaac030a668a09e398063c856b0c374c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-01-27 16:34:17 -0500

    Disable padding-line-between-statements for prose-not-found, per issue reported: https://github.com/NullVoxPopuli/ember-eslint-parser/pull/35

--- apps/tutorial/.eslintrc.cjs
+++ apps/tutorial/.eslintrc.cjs
@@ -18,5 +18,12 @@ module.exports = {
       plugins: ['ember'],
       parser: 'ember-eslint-parser',
     },
+    {
+      files: ['app/components/prose/prose-not-found.gts'],
+      rules: {
+        // https://github.com/NullVoxPopuli/ember-eslint-parser/pull/35
+        'padding-line-between-statements': 'off',
+      },
+    },
   ],
 };
