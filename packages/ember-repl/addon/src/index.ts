75589951c1b7dd004ab58db4d44d5de3583dbac4
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-12 13:33:32 -0400

    Re-export some types from ember-repl

--- packages/ember-repl/addon/src/index.ts
+++ packages/ember-repl/addon/src/index.ts
@@ -2,4 +2,4 @@ export { compile, Compiled } from './compile/index.ts';
 export { invocationName, invocationOf, nameFor } from './compile/utils.ts';
 
 // Public Types
-export type { CompileResult } from './compile/types';
+export type { CompileResult, EvalImportMap,ScopeMap, UnifiedPlugin } from './compile/types';
