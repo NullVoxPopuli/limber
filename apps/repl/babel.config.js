a2b3620ee2806b85b943aaea514cd2c7cdeda30c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-23 15:08:37 -0400

    Fix codemirror reference -- now @babel/runtime is missing

--- apps/repl/babel.config.js
+++ apps/repl/babel.config.js
@@ -1,4 +1,17 @@
 // eslint-disable-next-line n/no-missing-require
 let config = require('./node_modules/.embroider/rewritten-app/_babel_config_');
 
-module.exports = config;
+module.exports = {
+  ...config,
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry',
+        corejs: '3.22',
+      },
+    ],
+  ],
+};
+
+// console.log(module.exports);
