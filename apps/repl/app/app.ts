b6b0753838fc447ef79da081ae9262e6b5c7b1f3
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-03-23 17:40:48 -0400

    Looks like I need to wait until the rewritten app and stuff is removed? all errors point *around* there, which gives me doubts

--- apps/repl/app/app.ts
+++ apps/repl/app/app.ts
@@ -1,12 +1,8 @@
+import './css/tailwind.css';
+import './css/app.css';
 import 'limber-ui/theme.css';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { DEBUG } from '@glimmer/env';
 import Application from '@ember/application';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { _backburner } from '@ember/runloop';
 
 import loadInitializers from 'ember-load-initializers';
 import Resolver from 'ember-resolver';
@@ -14,16 +10,6 @@ import { setupComponentMachines } from 'ember-statechart-component';
 import { StateNode } from 'xstate';
 
 import config from 'limber/config/environment';
-
-if (DEBUG) {
-  // This has performance implications, but the debuggability is worth it.
-  // Debugging eventloop stuff is notoriously difficult, so let's make it
-  // not difficult by default.
-  //
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  _backburner.DEBUG = true;
-}
 
 // @babel/traverse (from babel-plugin-ember-template-imports)
 // accesses process.....
---
+++ apps/repl/tailwind.config.mjs
@@ -0,0 +1,3 @@
+import { config } from "@nullvoxpopuli/limber-styles";
+
+export default config(import.meta);
