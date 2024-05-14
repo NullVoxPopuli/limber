80dc394004e21712b3a0c9a94312e7182920a195
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-05-14 16:35:51 -0400

    Upgrade template-lint to test mjs files

---
+++ apps/tutorial/.template-lintrc.mjs
@@ -0,0 +1,19 @@
+export default {
+  extends: "recommended",
+  ignore: ["public/docs/**/*"],
+  overrides: [
+    {
+      files: ["**/*.gts", "**/*.gjs"],
+      rules: {
+        // Handled by ESLint
+        //   otherwise gives false negatives
+        "no-implicit-this": "off",
+        // false negatives due to being defined in js-scope
+        "no-curly-component-invocation": "off",
+        // We do what we want. psh
+        "no-forbidden-elements": "off",
+        "no-inline-styles": "off",
+      },
+    },
+  ],
+};
