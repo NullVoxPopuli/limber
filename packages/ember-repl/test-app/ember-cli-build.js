4c6ff99823146ba521d7044637fbd8a36c5ca915
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-10-06 00:52:33 -0400

    Fix repl tests

--- packages/ember-repl/test-app/ember-cli-build.js
+++ packages/ember-repl/test-app/ember-cli-build.js
@@ -20,6 +20,8 @@ module.exports = function (defaults) {
     },
   });
 
+  app.import('vendor/ember/ember-template-compiler.js');
+
   const { maybeEmbroider } = require('@embroider/test-setup');
 
   return maybeEmbroider(app, {
