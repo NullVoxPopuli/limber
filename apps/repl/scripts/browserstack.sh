47d972592957ca84d84b5af9ea99b388fbd7f5aa
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-27 04:20:37 -0500

    Fix browserstack script

--- apps/repl/scripts/browserstack.sh
+++ apps/repl/scripts/browserstack.sh
@@ -8,8 +8,8 @@ function cleanup()
 trap cleanup EXIT
 trap cleanup SIGINT
 
-pnpm ember build --environment=development
+pnpm vite build --mode test
 pnpm ember browserstack:connect
-pnpm ember test --path ./dist --test-port=7774 --host 127.0.0.1 --config-file=browserstack.testem.js
+pnpm ember test --path ./dist --test-port=7774 --host 127.0.0.1 --config-file=browserstack.testem.cjs
 pnpm ember browserstack:results
 
