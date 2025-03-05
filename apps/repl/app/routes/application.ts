07556b95a667f7a3eea56cb7134e38ecd77d7a1b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-03-04 22:59:10 -0500

    Oh my

--- apps/repl/app/routes/application.ts
+++ apps/repl/app/routes/application.ts
@@ -2,9 +2,16 @@ import Route from '@ember/routing/route';
 
 import { setupTabster } from 'ember-primitives/tabster';
 
+const map = new WeakSet();
+
 export default class ApplicationRoute extends Route {
-  async beforeModel() {
-    setupTabster(this);
+  // The router is littered with bugs.. so here we only let the model hook run once per instance
+  model() {
+    if (!map.has(this)) {
+      setupTabster(this);
+      map.add(this);
+    }
+
     document.querySelector('#initial-loader')?.remove();
   }
 }
