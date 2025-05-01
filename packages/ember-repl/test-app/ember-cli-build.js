44cdc4c1317a92f8dd45ab0a722a9da27c7494f5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-01 10:04:56 -0400

    ope

--- packages/ember-repl/test-app/ember-cli-build.js
+++ packages/ember-repl/test-app/ember-cli-build.js
@@ -1,35 +1,15 @@
-'use strict';;
+'use strict';
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const path = require('path');
 
-const {
-  compatBuild
-} = require("@embroider/compat");
+const { compatBuild } = require('@embroider/compat');
 
-module.exports = async function(defaults) {
-  const {
-    buildOnce
-  } = await import("@embroider/vite");
+module.exports = async function (defaults) {
+  const { buildOnce } = await import('@embroider/vite');
 
-  const sideWatch = require('@embroider/broccoli-side-watch');
-
-  const app = new EmberApp(defaults, {
-    // Add options here
-    trees: {
-      app: sideWatch('app', { watching: [path.join(__dirname, '../addon/dist')] }),
-    },
-    'ember-cli-babel': {
-      enableTypeScriptTransform: true,
-    },
-    name: 'ember-repl-test-app',
-    autoImport: {
-      watchDependencies: Object.keys(require('./package.json').dependencies),
-    },
-  });
+  const app = new EmberApp(defaults, {});
 
   app.import('vendor/ember/ember-template-compiler.js');
-
-  const { maybeEmbroider } = require('@embroider/test-setup');
 
   return compatBuild(app, buildOnce);
 };
--- packages/ember-repl/test-app/vite.config.mjs
+++ packages/ember-repl/test-app/vite.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import { extensions, classicEmberSupport, ember } from '@embroider/vite';
-import { babel } from '@rollup/plugin-babel';
+import { classicEmberSupport, ember, extensions } from "@embroider/vite";
+
+import { babel } from "@rollup/plugin-babel";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     classicEmberSupport(),
     ember(),
-    // extra plugins here
     babel({
-      babelHelpers: 'runtime',
+      babelHelpers: "runtime",
       extensions,
     }),
   ],
