87e8758596debbf7ac7ed8c63bc7af5f6db8b644
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-04 10:46:07 -0400

    lints

--- packages/syntax/glimdown/codemirror/src/index.ts
+++ packages/syntax/glimdown/codemirror/src/index.ts
@@ -107,6 +107,7 @@ export function glimdown() {
         alias: ['hbs', 'glimmer', 'ember', 'handlebars'],
         extensions: ['hbs'],
         async load() {
+          // @ts-ignore
           const { glimmer } = await import('codemirror-lang-glimmer');
 
           return glimmer();
@@ -117,7 +118,7 @@ export function glimdown() {
         alias: ['gjs', 'glimmer-js', 'javascript.glimmer'],
         extensions: ['gjs'],
         async load() {
-          // @ts-expect-error
+          // @ts-ignore
           const { gjs } = await import('codemirror-lang-glimmer-js');
 
           return gjs();
@@ -128,7 +129,7 @@ export function glimdown() {
         alias: ['gts', 'glimmer-ts', 'typescript.glimmer'],
         extensions: ['gts'],
         async load() {
-          // @ts-expect-error
+          // @ts-ignore
           const { gts } = await import('codemirror-lang-glimmer-js');
 
           return gts();
