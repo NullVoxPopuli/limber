e09f02e64954d4aa2bd2a2e32b1796025c0eb97e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-05 19:28:29 -0400

    Copy the main things from #1881

---
+++ packages/repl-sdk/src/index.d.ts
@@ -0,0 +1,19 @@
+import type { CompilerConfig, Options } from './types.ts';
+
+export const defaultFormats: keyof Options['formats'];
+export const defaults: Options;
+
+export class Compiler {
+  constructor(options?: Options);
+
+  compile(
+    format: string,
+    text: string,
+    options?: {
+      flavor?: string;
+      fileName?: string;
+    }
+  ): Promise<HTMLElement>;
+
+  optionsFor(format: string, flavor?: string): Omit<CompilerConfig, 'compiler'>;
+}
