9aed3b7ec1b2b4633e89fdf732aa29c3c2ade11b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-05 02:13:43 -0400

    Update jsx-react-demo.jsx

--- apps/repl/public/samples/jsx-react-demo.jsx
+++ apps/repl/public/samples/jsx-react-demo.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// NOTE: https://github.com/fkhadra/react-toastify/issues/1246
-//       I had to disable shadowdom for this demo
-//       You can, too, with &shadowdom=0 in the URL
 import { ToastContainer, toast } from 'react-toastify';
 
 function Demo() {
