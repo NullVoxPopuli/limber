89ff4382333ab809ae5b29daa926c9ccf297d6ab
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-05 14:37:01 -0400

    Fix build (for distro) of ember-repl

--- packages/ember-repl/config/rollup.config.mjs
+++ packages/ember-repl/config/rollup.config.mjs
@@ -1,27 +1,26 @@
-import { babel } from '@rollup/plugin-babel';
-import { Addon } from '@embroider/addon-dev/rollup';
-import { resolve, dirname } from 'node:path';
+import { babel } from "@rollup/plugin-babel";
+import { Addon } from "@embroider/addon-dev/rollup";
+import { resolve } from "node:path";
 import cjs from "@rollup/plugin-commonjs";
 import nodePolyfills from "rollup-plugin-node-polyfills";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const addon = new Addon({
-  srcDir: 'src',
-  destDir: 'dist',
+  srcDir: "src",
+  destDir: "dist",
 });
 
-const babelConfig = resolve(import.meta.dirname, './babel.publish.config.cjs');
-const tsConfig = resolve(import.meta.dirname, './tsconfig.publish.json');
+const babelConfig = resolve(import.meta.dirname, "./babel.publish.config.cjs");
+const tsConfig = resolve(import.meta.dirname, "./tsconfig.publish.json");
 
 export default {
   output: addon.output(),
-
   plugins: [
-    addon.publicEntrypoints(['**/*.js']),
+    addon.publicEntrypoints(["**/*.js"]),
     addon.dependencies(),
     babel({
-      extensions: ['.js', '.gjs', '.ts', '.gts'],
-      babelHelpers: 'bundled',
+      extensions: [".js", ".gjs", ".ts", ".gts"],
+      babelHelpers: "bundled",
       configFile: babelConfig,
     }),
     nodeResolve({
@@ -32,14 +31,11 @@ export default {
     }),
     // This is needed because babel doesn't ship a proper browser bundle...
     nodePolyfills(),
-    // line-column...
+    // line-column, unified (extend)...
     cjs(),
 
     // Emit .d.ts declaration files
-    addon.declarations(
-      'declarations',
-      `npx glint --declaration --project ${tsConfig}`,
-    ),
+    addon.declarations("declarations", `pnpm glint --declaration --project ${tsConfig}`),
 
     // Remove leftover build artifacts when starting a new build.
     addon.clean(),
