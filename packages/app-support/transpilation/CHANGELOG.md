515be692845208b67435a4e095171dee5cc57ece
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-16 18:40:36 -0400

    Delete stuff

--- packages/app-support/transpilation/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-# @nullvoxpopuli/limber-transpilation
-
-## 0.0.1
--- packages/app-support/transpilation/eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import { configs } from "@nullvoxpopuli/eslint-configs";
-
-// eslint-disable-next-line n/no-unsupported-features/node-builtins
-export default configs.node(import.meta.dirname);
