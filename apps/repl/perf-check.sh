78550fe750647c67554370f33a169b19248c2af1
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-08-09 15:49:52 -0400

    ope

--- apps/repl/perf-check.sh
+++ apps/repl/perf-check.sh
@@ -14,6 +14,9 @@ echo "Measuring build perf of $(basename $PWD)"
 #
 #
 #  (Setting to 1 means use this exact path)
+
+TOOL="pnpm dlx build-start-rebuild-perf"
+
 if [[ -v LOCAL ]]; then
   if [[ "$LOCAL" == "1" ]]; then
     relative="Development/OpenSource/build-start-rebuild-perf/bin/cli.js"
@@ -21,16 +24,40 @@ if [[ -v LOCAL ]]; then
     echo "Using default location of ~/$relative"
   fi
 
-	node $LOCAL \
-		--file ./app/app.js \
-		--wait-for "main header nav" \
-		--url "https://localhost:4201/" \
-		--log-level log
-else
-	# Docs: https://github.com/mainmatter/build-start-rebuild-perf
-	pnpm dlx build-start-rebuild-perf \
-		--file ./app/app.js \
-		--wait-for "main header nav" \
-		--url "https://localhost:4201/" \
-		--log-level log
+  TOOL="node $LOCAL"
 fi
+
+port="41137"
+
+common_args="
+  --timeout 180000 \
+  --file ./app/app.ts \
+  --wait-for 'main header nav' \
+  --url 'https://localhost:$port/' \
+  $@
+"
+
+
+with_force="
+  $TOOL \
+    --command 'pnpm vite --force --port $port' \
+    $common_args
+"
+
+warm="
+  $TOOL \
+    --command 'pnpm vite --port $port' \
+    $common_args
+"
+
+echo "No Cache"
+
+echo "Running:"
+echo -e "$with_force"
+eval $with_force
+
+echo "Warm"
+
+echo "Running:"
+echo -e "$warm"
+eval $warm
