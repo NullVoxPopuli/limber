8df0f5e5ab265818a3e0b529f73e94d111167941
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-01 19:23:08 -0500

    ope

--- packages/ember-repl/config/vite.config.mjs
+++ packages/ember-repl/config/vite.config.mjs
@@ -1,35 +1,35 @@
-import { ember, extensions } from "@embroider/vite";
-import { join } from "node:path";
+import { ember, extensions } from '@embroider/vite';
+import { join } from 'node:path';
 
-import { babel } from "@rollup/plugin-babel";
-import { defineConfig } from "vite";
-import mkcert from "vite-plugin-mkcert";
+import { babel } from '@rollup/plugin-babel';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
   build: {
-    target: ["esnext"],
+    target: ['esnext'],
   },
   resolve: {
     alias: [
       {
-        find: "ember-repl",
+        find: 'ember-repl',
         replacement: join(import.meta.dirname, `../src`),
       },
     ],
   },
   optimizeDeps: {
     // a wasm-providing dependency
-    exclude: ["content-tag"],
+    exclude: ['content-tag'],
     // for top-level-await, etc
     esbuildOptions: {
-      target: "esnext",
+      target: 'esnext',
     },
   },
   plugins: [
     mkcert(),
     ember(),
     babel({
-      babelHelpers: "inline",
+      babelHelpers: 'inline',
       extensions,
     }),
   ],
