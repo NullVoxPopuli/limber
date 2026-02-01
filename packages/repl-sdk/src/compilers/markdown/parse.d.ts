d8a8bc4eb8977fda9e8499325848e6b4d80acdbe
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-01 12:35:15 -0500

    expose the markdown compiler

---
+++ packages/repl-sdk/src/compilers/markdown/parse.d.ts
@@ -0,0 +1,11 @@
+import type { InternalOptions } from "./types";
+
+export function parseMarkdown(input: string, options: InternalOptions): Promise<{
+    text: string;
+    codeBlocks: Array<{
+        lang: string;
+        format: string;
+        code: string;
+        name: string;
+    }>
+}>
