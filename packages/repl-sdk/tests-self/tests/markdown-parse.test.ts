386d2da099943257269a073f73498a6b828a8c2b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-01 19:43:19 -0500

    ope

--- packages/repl-sdk/tests-self/tests/markdown-parse.test.ts
+++ packages/repl-sdk/tests-self/tests/markdown-parse.test.ts
@@ -3,9 +3,7 @@ import { parseMarkdown } from 'repl-sdk/markdown/parse';
 
 describe('markdown/parse', () => {
   test('it works', async () => {
-    const result = await parseMarkdown(`# hello`, {
-        
-    });
+    const result = await parseMarkdown(`# hello`, {});
 
     expect(result.text).toMatchInlineSnapshot(`"<h1 id="hello">hello</h1>"`);
     expect(result.codeBlocks).toMatchInlineSnapshot(`[]`);
